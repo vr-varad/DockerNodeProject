1e4ee5345893d6e67a016c503e7582e74207446f
Varad Gupta <varadgupta21@gmail.com>
2024-02-24 00:27:33 +0530

    added env

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.port;
+const port = process.env.PORT;
 
 app.get('/home', (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
-
 app.listen(port, () => {
     console.log('Server is running on port',port);
 });
